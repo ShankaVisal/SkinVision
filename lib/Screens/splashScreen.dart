aec788e3d01a825eb0d212b5db1189dc3ebe3ee7
Shanka Visal <shankavisal@gmail.com>
2024-04-26 22:32:43 +0530

    Initial commit

---
+++ lib/Screens/splashScreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: Image(image: AssetImage('assets/splashscreen.png')),
+          ),
+          Positioned(
+            bottom: height * 0.01,
+            left: width * 0.3,
+            right: width * 0.3,
+            child: SizedBox(
+              // width: width/2,
+              child: Lottie.network(
+                  'https://lottie.host/db0a1cfa-ad7e-431b-90a2-993c15f3a4fb/0fPD80BKdk.json'),
+            ),
+          ),
+          SizedBox(
+            height: height,
+            width: width*0.55,
+            child: Image(image: AssetImage('assets/logo.png')),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
