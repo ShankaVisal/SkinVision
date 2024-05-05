a0c18888d61c278c338a32c20ecc033a088b7b59
Sanuda Senod <sanudasenod446@gmail.com>
2024-05-06 00:42:10 +0530

    master to master (#5)
    * firebase configuration done (#1)
    
    * minor changes
    
    * Dev (#2)
    
    * splash screen 2 done
    
    * login & logout UI done
    
    ---------
    
    Co-authored-by: Sanuda Senod <sanudasenod446@gmail.com>
    
    * authentication done
    
    * HomePage done without BottomNavigation
    
    * restructred the code base
    
    ---------
    
    Co-authored-by: Shanka Visal <shankavisal@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled5/Screens/splashScreen.dart';
 import 'package:untitled5/controllers/firebase_options.dart';
-import 'package:untitled5/home_screen_page.dart';
+import 'package:untitled5/Pages/home_screen_page.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
