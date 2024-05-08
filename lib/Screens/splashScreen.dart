b2fcbcb3fc7e42c792403a39035db5e40d4e8c1c
Shanka Visal <95647848+ShankaVisal@users.noreply.github.com>
2024-05-09 00:11:40 +0530

    Test (#6)
    * Dev (#2)
    
    * splash screen 2 done
    
    * login & logout UI done
    
    ---------
    
    Co-authored-by: Shanka Visal <shankavisal@gmail.com>
    
    * authentication done
    
    * HomePage done without BottomNavigation
    
    * restructred the code base (#4)
    
    ---------
    
    Co-authored-by: Sanuda Senod <sanudasenod446@gmail.com>

--- lib/Screens/splashScreen.dart
+++ lib/Screens/splashScreen.dart
@@ -15,7 +15,9 @@ class _splashScreenState extends State<splashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(Duration(seconds: 4), () {
+
+    Future.delayed(const Duration(seconds: 4), () {
+
 
       Navigator.push(
         context,
@@ -34,7 +36,9 @@ class _splashScreenState extends State<splashScreen> {
           SizedBox(
             height: height,
             width: width,
-            child: Image(image: AssetImage('assets/1.png')),
+
+            child: const Image(image: AssetImage('assets/1.png')),
+
 
           ),
           Positioned(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled5/Screens/splashScreen.dart';
+import 'package:untitled5/controllers/firebase_options.dart';
+import 'package:untitled5/Pages/home_screen_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,12 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 239, 168, 133)),
         useMaterial3: true,
       ),
-      home: const splashScreen(),
+      home: const  HomeScreenPage()/*splashScreen()*/,
     );
   }
 }
