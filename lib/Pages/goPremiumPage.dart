ed332cfa742250c455c4a78a4ba622d48c8d2cd7
Shanka Visal <95647848+ShankaVisal@users.noreply.github.com>
2024-05-17 01:58:32 +0530

    premium page done (#11)
    * Dev (#2)
    
    * splash screen 2 done
    
    * login & logout UI done
    
    ---------
    
    Co-authored-by: Shanka Visal <shankavisal@gmail.com>
    
    * authentication done
    
    * HomePage done without BottomNavigation
    
    * Test (#3)
    
    * firebase configuration done (#1)
    
    * minor changes
    
    * Dev (#2)
    
    * splash screen 2 done
    
    * login & logout UI done
    
    ---------
    
    Co-authored-by: Shanka Visal <shankavisal@gmail.com>
    
    * authentication done
    
    * HomePage done without BottomNavigation
    
    ---------
    
    Co-authored-by: Shanka Visal <shankavisal@gmail.com>
    
    * restructred the code base
    
    * skin scan page dev started
    
    * dermatologistsPage half done
    
    * Merge pull request #2 from ShankaVisal/Dermatologists (#7)
    
    * needs to add routing (#8)
    
    * product catlog done
    
    * premium page done
    
    ---------
    
    Co-authored-by: Sanuda Senod <sanudasenod446@gmail.com>
    Co-authored-by: Sanuda Senod <ypsspathiraja@std.appsc.sab.ac.lk>

--- lib/Pages/goPremiumPage.dart
+++ lib/Pages/goPremiumPage.dart
@@ -10,6 +10,225 @@ class GoPremiumPage extends StatefulWidget {
 class _GoPremiumPageState extends State<GoPremiumPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Premium Version'),
+          ),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 254, 238, 232),
+                  Color.fromARGB(255, 240, 228, 252)
+                ],
+              ),
+            ),
+            child: SafeArea(
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30,right: 30),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Monthly Summery',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Advance analysis report',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Ad free',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Advanced Product Recommendations',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Advanced Analysis Report',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(width: 30,child: Image.asset('assets/done.png'),),
+                              SizedBox(width: 5,),
+                              Text('Contact doctor through the application',style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Divider(
+                        height: 1,
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "\$ 1.99",
+                          style: TextStyle(
+                              fontSize: 36,
+                              color: Color(0xff027A76),
+                              fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 11,
+                            ),
+                            Text(
+                              "/ month",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xff027A76),
+                                  fontWeight: FontWeight.w500
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      width: width*0.8,
+                      child: FloatingActionButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        backgroundColor: Colors.black,
+                        onPressed: (){},
+                        child: Text(
+                          'Get Premium',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          bottomNavigationBar: SizedBox(
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    _buildIconButton("assets/IconImages/HomeIcon.png"),
+                    _buildIconButton("assets/IconImages/userIcon.png"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _buildIconButton("assets/IconImages/infoIcon.png"),
+                    _buildIconButton("assets/IconImages/notificationIcon.png"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 35,
+          left: MediaQuery.of(context).size.width / 2 - 35,
+          child: SizedBox(
+            width: 70,
+            height: 70,
+            child: FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Image.asset(
+                "assets/IconImages/scan_Icon.png",
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ]
+
+    );
   }
+}
+
+Widget _buildIconButton(String assetPath) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 18.0),
+    child: IconButton(
+      icon: Image.asset(
+        assetPath,
+        width: 30,
+        height: 30,
+      ),
+      onPressed: () {},
+    ),
+  );
 }
